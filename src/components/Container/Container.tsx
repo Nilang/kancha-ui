fe85a1d98433afc5d5e0cc4c6ecb66077852f20a
Jason Healy <hello@jasonhealy.io>
2019-06-03 19:35:01 +0100

    Set up local build

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { View } from 'react-native'
+
+interface ContainerProps {
+  param: string
+}
+
+const Container: React.FC<ContainerProps> = props => {
+  return <View />
+}
+
+export default Container
