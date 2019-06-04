855615ef23c16949dafef0b4de4585adc50528c6
Jason Healy <hello@jasonhealy.io>
2019-06-04 11:02:14 +0100

    Add readme

--- demo/KanchaSink/src/screens/App.tsx
+++ demo/KanchaSink/src/screens/App.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
+import { Container } from 'kancha'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 })
 
 interface Props {}
@@ -11,6 +14,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Container param="TEST" />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
