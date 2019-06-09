215daf35eef6c272439ba6797885aa4500ee56d2
Jason Healy <hello@jasonhealy.io>
2019-06-09 15:03:45 +0100

    test(button): Add tests to bring button coverage to 100%

--- src/components/Button/__tests__/Button.snapshot.test.tsx
+++ src/components/Button/__tests__/Button.snapshot.test.tsx
@@ -3,10 +3,47 @@ import { render } from 'react-native-testing-library'
 
 import Button from '../Button'
 
-describe('Container', () => {
-  it('renders correctly', () => {
+describe('Component: Button', () => {
+  it('renders a filled button correctly with all props', () => {
     const onPress = jest.fn()
-    const tree = render(<Button onPress={onPress} />).toJSON()
+    const tree = render(
+      <Button
+        type={'primary'}
+        block={'filled'}
+        bold={true}
+        fullWidth={true}
+        centered={true}
+        onPress={onPress}
+        icon={<React.Fragment />}
+        noPadding={true}
+        textDecorationLine={'none'}
+        testID={'TEST_ID'}
+        depth={1}
+      />,
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders an outlined button correctly', () => {
+    const onPress = jest.fn()
+    const tree = render(<Button onPress={onPress} block={'outlined'} icon={<React.Fragment />} />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders an outlined button correctly with a type', () => {
+    const onPress = jest.fn()
+    const tree = render(
+      <Button type={'primary'} onPress={onPress} block={'outlined'} icon={<React.Fragment />} />,
+    ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders an icon button correctly', () => {
+    const onPress = jest.fn()
+    const tree = render(<Button onPress={onPress} icon={<React.Fragment />} iconButton={true} />).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
@@ -21,6 +58,13 @@ describe('Container', () => {
   it('renders a disabled button correctly', () => {
     const onPress = jest.fn()
     const tree = render(<Button onPress={onPress} disabled={true} />).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a disabled nav button correctly', () => {
+    const onPress = jest.fn()
+    const tree = render(<Button onPress={onPress} disabled={true} navButton={true} />).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
