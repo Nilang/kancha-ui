28e4fc413767b19660eaae6dfe8a060f504a49f0
Jason Healy <hello@jasonhealy.io>
2019-06-09 01:02:26 +0100

    chore(build): Removed dist from repo

--- dist/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-interface ButtonProps {
-    onPress: () => void;
-}
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- dist/theming/index.js
+++
@@ -1,4 +0,0 @@
-import { createTheming } from '@callstack/react-theme-provider';
-import defaultTheme from './theme';
-const { ThemeProvider, withTheme, useTheme } = createTheming(defaultTheme);
-export { ThemeProvider, withTheme, useTheme };
