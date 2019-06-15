a4e66bdbe1ca90f24cf0d26f41d98696e8e9a315
Jason Healy <hello@jasonhealy.io>
2019-06-15 16:04:43 +0100

    test: Add new button + state e2e test

--- demo/src/screens/App.tsx
+++ demo/src/screens/App.tsx
@@ -1,30 +1,42 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Container, Text, Constants, Button } from '@kancha/kancha-ui'
 
-interface Props {}
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <Container
-        flex={1}
-        justifyContent={'center'}
-        background={Constants.BrandOptions.Primary}
-        testID="WELCOME_TESTID"
-      >
-        <Text bold={true} textAlign={'center'} type={Constants.TextTypes.H3}>
-          🚀Hey, It's Kancha Demo! 🎉
-        </Text>
+import TEST_ID from '../../../e2e/testIDs'
 
-        <Container paddingTop={50} paddingHorizontal={30}>
-          <Button
-            fullWidth={true}
-            onPress={() => ''}
-            buttonText={'Tap me'}
-            type={Constants.BrandOptions.Primary}
-            block={Constants.ButtonBlocks.Filled}
-          />
-        </Container>
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  const [isVisible, toggleVisible] = useState(false)
+
+  return (
+    <Container
+      flex={1}
+      justifyContent={'center'}
+      background={Constants.BrandOptions.Primary}
+      testID={TEST_ID.WELCOME}
+    >
+      <Text bold={true} textAlign={'center'} type={Constants.TextTypes.H3}>
+        🚀Hey, It's Kancha Demo! 🎉
+      </Text>
+
+      <Container paddingTop={50} paddingHorizontal={30}>
+        <Button
+          testID={TEST_ID.WELCOME_BUTTON}
+          fullWidth={true}
+          onPress={() => toggleVisible(!isVisible)}
+          buttonText={'Tap me'}
+          type={Constants.BrandOptions.Primary}
+          block={Constants.ButtonBlocks.Filled}
+        />
       </Container>
-    )
-  }
+
+      {isVisible && (
+        <Container paddingHorizontal={30}>
+          <Text testID={TEST_ID.VISIBLE_TEXT}>Hello, I'm visible now!</Text>
+        </Container>
+      )}
+    </Container>
+  )
 }
+
+export default App
