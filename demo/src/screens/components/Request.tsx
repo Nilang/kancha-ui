29293a525472e4618486b6e9dbe3151a39e882b6
Jason Healy <hello@jasonhealy.io>
2019-08-27 12:48:08 +0100

    docs: Update Request demo

--- demo/src/screens/components/Request.tsx
+++ demo/src/screens/components/Request.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react'
-import { Container, Screen, Button, Constants, Banner, RequestItem } from '@kancha/kancha-ui'
+import {
+  Container,
+  Screen,
+  Button,
+  Constants,
+  Banner,
+  RequestItem,
+  Typings,
+  Indicator,
+} from '@kancha/kancha-ui'
 import { NavigationScreen } from '../../navigators'
 import { Colors } from '../../theme'
 
@@ -10,6 +19,112 @@ const avatar1 = require('../../assets/images/kitten-avatar.jpg')
 const bannerImage = require('../../assets/images/abstract-blurred-gradient.jpg')
 
 import TEST_ID from '../../../../e2e/testIDs'
+
+const nameOptions: Typings.RequestItemSelectable[] = [
+  {
+    id: '0001',
+    iss: 'Deutsche Bank',
+    property: 'name',
+    value: 'Jack',
+    selected: true,
+  },
+  {
+    id: '0002',
+    iss: 'Onfido',
+    property: 'name',
+    value: 'Jacky',
+    selected: false,
+  },
+  {
+    id: '0003',
+    iss: 'Onfido',
+    property: 'name',
+    value: 'Jacko',
+    selected: false,
+  },
+]
+
+const lastNameOptions: Typings.RequestItemSelectable[] = [
+  {
+    id: '0001',
+    iss: 'Deutsche Bank',
+    property: 'lastName',
+    value: 'Morrison',
+    selected: true,
+  },
+  {
+    id: '0002',
+    iss: 'Onfido',
+    property: 'lastName',
+    value: 'Kennedy',
+    selected: false,
+  },
+  {
+    id: '0003',
+    iss: 'Onfido',
+    property: 'lastName',
+    value: 'Morrison',
+    selected: false,
+  },
+]
+
+const locationOptions: Typings.RequestItemSelectable[] = [
+  {
+    id: '0001',
+    iss: 'Deutsche Bank',
+    property: 'location',
+    value: 'Ireland',
+    selected: true,
+  },
+  {
+    id: '0002',
+    iss: 'Onfido',
+    property: 'location',
+    value: 'Dublin, Ireland',
+    selected: false,
+  },
+  {
+    id: '0003',
+    iss: 'Onfido',
+    property: 'location',
+    value: 'Dublin',
+    selected: false,
+  },
+]
+
+const emailOptions: Typings.RequestItemSelectable[] = [
+  {
+    id: '0001',
+    iss: 'Deutsche Bank',
+    property: 'location',
+    value: 'jack@mymail.com',
+    selected: true,
+  },
+  {
+    id: '0002',
+    iss: 'Onfido',
+    property: 'name',
+    value: 'jacky@mymail.com',
+    selected: false,
+  },
+  {
+    id: '0003',
+    iss: 'Serto Verified',
+    property: 'name',
+    value: 'jackie_1234@mymail.com',
+    selected: false,
+  },
+]
+
+const phoneOptions: Typings.RequestItemSelectable[] = [
+  {
+    id: '0001',
+    iss: 'Serto Verified',
+    property: 'phone',
+    value: '+555-321-8763',
+    selected: true,
+  },
+]
 
 const Component: React.FC<NavigationScreen> = ({ navigation }) => {
   return (
@@ -34,7 +149,7 @@ const Component: React.FC<NavigationScreen> = ({ navigation }) => {
               <Button
                 block={Constants.ButtonBlocks.Filled}
                 type={Constants.BrandOptions.Primary}
-                buttonText={'Accept'}
+                buttonText={'Share'}
                 onPress={() => navigation.goBack()}
                 shadowOpacity={0.2}
               />
@@ -45,18 +160,18 @@ const Component: React.FC<NavigationScreen> = ({ navigation }) => {
     >
       <Container testID={TEST_ID.WELCOME}>
         <Banner
-          title={'Onfido ID'}
-          subTitle={'Hello subtitle'}
+          title={'Crypto World Conf'}
+          subTitle={'HODL Inc'}
           avatar={avatar1}
           backgroundImage={bannerImage}
         />
+        <Indicator text={'Share your data with HODL Inc.'} />
         <Container>
-          <RequestItem subTitle={'Name'}>Jack</RequestItem>
-          <RequestItem subTitle={'Date of Birth'}>24-01-1991</RequestItem>
-          <RequestItem subTitle={'Location'}>Ireland</RequestItem>
-          <RequestItem subTitle={'Phone'} itemNote={'Mobile'}>
-            555-876-882771
-          </RequestItem>
+          <RequestItem subTitle={'Firstname'} options={nameOptions} required={true} />
+          <RequestItem subTitle={'Lastname'} options={lastNameOptions} required={true} />
+          <RequestItem subTitle={'Location'} options={locationOptions} required={true} />
+          <RequestItem subTitle={'Email'} options={emailOptions} required={false} />
+          <RequestItem subTitle={'Phone'} options={phoneOptions} required={false} />
         </Container>
       </Container>
     </Screen>
