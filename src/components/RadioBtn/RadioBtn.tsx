73ed68d178ace6463cc0a27927db18ac1ade8519
Jason Healy <hello@jasonhealy.io>
2019-08-27 12:46:22 +0100

    feat: Added RadioBtn component

---
+++ src/components/RadioBtn/RadioBtn.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { TouchableHighlight } from 'react-native'
+import Container from '../Container/Container'
+import Icon from '../Icon/Icon'
+import Text from '../Text/Text'
+import { withTheme } from '../../theming'
+
+interface RadioProps {
+  /**
+   * On Press handler
+   */
+  onPress: () => void
+
+  /**
+   * Selected flag
+   */
+  selected: boolean
+
+  /**
+   * Disable the button
+   */
+  disabled?: boolean
+
+  /**
+   * Theme
+   */
+  theme: any
+}
+
+const Radio: React.FC<RadioProps> = props => {
+  return (
+    <TouchableHighlight onPress={props.onPress} underlayColor={'transparent'} disabled={props.disabled}>
+      <Container flexDirection={'row'} alignItems={'center'} marginBottom={5}>
+        <Icon icon={props.selected ? props.theme.icons.RADIO_ON : props.theme.icons.RADIO_OFF} />
+        <Container flex={1} paddingLeft={8}>
+          <Text>{props.children}</Text>
+        </Container>
+      </Container>
+    </TouchableHighlight>
+  )
+}
+
+export default withTheme(Radio)
