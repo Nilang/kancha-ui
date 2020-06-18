121437de7be0ad31673e68694c06b673d8a33015
Jason Healy <hello@jasonhealy.io>
2020-06-18 16:24:14 +0100

    test: Update snapshots

--- src/theming/index.ts
+++ src/theming/index.ts
@@ -1,6 +1,6 @@
 import { createTheming } from '@callstack/react-theme-provider'
-import { createTheme, mergeTheme } from './theme'
+import { createTheme } from './theme'
 
 const { ThemeProvider, withTheme, useTheme } = createTheming(createTheme('light'))
 
-export { ThemeProvider, withTheme, useTheme, createTheme, mergeTheme }
+export { ThemeProvider, withTheme, useTheme, createTheme }
